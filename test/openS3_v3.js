c6d223ac94602c05d4246a70ca6768f249646f9c
catamphetamine <kuchumovn@gmail.com>
2026-06-22 06:47:53 +0300

    Resolved conflicts

--- test/openS3_v3.js
+++ test/openS3_v3.js
@@ -1,17 +1,12 @@
-<<<<<<< HEAD
 import { test } from "tap";
+import fs from "fs";
+import path from "path";
+import Stream from "stream";
 import { Open } from "../index.js";
 import { S3Client, GetObjectCommand, HeadObjectCommand } from "@aws-sdk/client-s3";
 
 global.GetObjectCommand = GetObjectCommand;
 global.HeadObjectCommand = HeadObjectCommand;
-=======
-const test = require("tap").test;
-const fs = require("fs");
-const path = require("path");
-const Stream = require("stream");
-const unzip = require("../unzip");
->>>>>>> aaf77f0c7b4d29af500b0aa9c0e2aa2ade0a2618
 
 const version = +process.version.replace("v", "").split(".")[0];
 
@@ -46,27 +41,13 @@ test(
   "get content of a single file entry out of a zip",
   { skip: version < 16 },
   function (t) {
-<<<<<<< HEAD
-    const client = new S3Client({
-      region: "us-east-1",
-      signer: { sign: async (request) => request },
-    });
-
-    // These files are provided by AWS's open data registry project.
-    // https://github.com/awslabs/open-data-registry
-
-    return Open.s3_v3(client, {
-      Bucket: "wikisum",
-      Key: "WikiSumDataset.zip",
-=======
     const archive = path.join(__dirname, "../testData/compressed-standard/archive.zip");
     const buffer = fs.readFileSync(archive);
     const client = createS3ClientMock(buffer);
 
-    return unzip.Open.s3_v3(client, {
+    return Open.s3_v3(client, {
       Bucket: "test",
       Key: "archive.zip",
->>>>>>> aaf77f0c7b4d29af500b0aa9c0e2aa2ade0a2618
     }).then(function (d) {
       const file = d.files.filter(function (file) {
         return file.path == "file.txt";
